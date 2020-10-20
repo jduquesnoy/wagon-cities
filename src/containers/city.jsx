cd8198131c20afd7f5e53f5ac031a7cfe2ead423
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-10-20 14:48:09 +0200

    3. CityList + City

---
+++ src/containers/city.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const City = (props) => {
+  return (
+    <li
+      className="list-group-item"
+    >{props.city.name}</li>
+  );
+};
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import City from './city';
+
+class CityList extends Component {
+ render() {
+return (
+  <div>
+ {this.props.cities.map((city) => {
+ return <City key={city.name} city={city} />;
+ })}
+ </div>
+ );
+ }
+}
+
+export default CityList;
